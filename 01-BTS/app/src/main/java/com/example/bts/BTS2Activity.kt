035a8c8be91543c2b359e1211e139dfbce59b081
rlarjsdn3 <rlarjsdn3@naver.com>
2025-01-19 15:07:39 +0900

    commit

---
+++ 01-BTS/app/src/main/java/com/example/bts/BTS2Activity.kt
@@ -0,0 +1,14 @@
+package com.example.bts
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BTS2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bts2)
+    }
+}
